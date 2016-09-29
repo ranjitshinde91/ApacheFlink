876dedbc4f6469f75249064e66e862e161425a00
Ranjit <ranjit.shinde@gslab.com>
2016-09-29 16:48:09 -0500

    log4j added

--- src/main/java/com/demo/flink/library/SnappyJava.java
+++ src/main/java/com/demo/flink/library/SnappyJava.java
@@ -1,22 +1,24 @@
 package com.demo.flink.library;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xerial.snappy.Snappy;
 
-
 public class SnappyJava {
+	private static Logger LOGGER = LoggerFactory.getLogger(SnappyJava.class);
 
 	public static void main(String[] args) throws IOException {
 		String input = "Hello snappy-java! Snappy-java is a JNI-based wrapper of "
 			     		+ "Snappy, a fast compresser/decompresser.";
-			byte[] compressed = Snappy.compress(input.getBytes("UTF-8"));
-			byte[] uncompressed =  Snappy.uncompress(compressed);
-			
-			System.out.println("compressed length :" +compressed.length);
-			System.out.println("uncompressed length :"+ uncompressed.length);
+		byte[] compressed = Snappy.compress(input.getBytes("UTF-8"));
+		byte[] uncompressed =  Snappy.uncompress(compressed);
+		
+		LOGGER.info("compressed length :" +compressed.length);
+		LOGGER.info("uncompressed length :"+ uncompressed.length);
 
-			String result = new String(uncompressed, "UTF-8");
-			System.out.println(result);
+		String result = new String(uncompressed, "UTF-8");
+		LOGGER.info(result);
 	}
 }
 
